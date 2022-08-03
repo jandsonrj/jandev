eacb94d3ed2fb09dd87c1f74b9eaa268c21f7d1c
JANDSON VITORINO <101944851+jandsonrj@users.noreply.github.com>
2022-08-03 10:22:52 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,48 @@
 $(document).ready(function(){
     $(window).scroll(function(){
+        // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
         }
+        
+        // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
         }else{
             $('.scroll-up-btn').removeClass("show");
-        }      
+        }
     });
-     $('.scroll-up-btn').click(function(){
+    // slide-up script
+    $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
-     });
-
-     var typed = new Typed(".typing", {
-        strings:["Desenvolvedor Web/>", "Suporte Técnico","Assistente Técnico", "Freelancer"],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true
-     });
-     var typed = new Typed(".typing-2", {
-        strings:["Desenvolvedor Web/>", "Suporte Técnico","Assistente Técnico", "Freelancer"],
-        typeSpeed:100,
-        backSpeed:60,
-        loop:true
-     });
-     var typed = new Typed(".wpp", {
+        // removing smooth scroll on slide-up button click
+        $('html').css("scrollBehavior", "auto");
+    });
+    $('.navbar .menu li a').click(function(){
+        // applying again smooth scroll on menu items click
+        $('html').css("scrollBehavior", "smooth");
+    });
+    // toggle menu/navbar script
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Desenvolvedor Web/>", "Suporte Técnico","Assistente Técnico", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Desenvolvedor Web/>", "Suporte Técnico","Assistente Técnico", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".wpp", {
         strings:["Ir para Whatsapp"],
         typeSpeed:100,
         backSpeed:80,
@@ -39,32 +54,27 @@ $(document).ready(function(){
         backSpeed:80,
         loop:true
      });
-
-    $('.menu-btn').click(function(){
-        $('.navbar .menu').toggleClass("active");
-        $('.menu-btn i').toggleClass("active");
-    });
+     
+    // owl carousel script
     $('.carousel').owlCarousel({
-        margin:20,
-        loop:true,
-        autoplayTimeOut:2000,
-        autoplayHoverPauser:true,
-        responsive:{
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
             0:{
-                items:1,
-                nav:false
+                items: 1,
+                nav: false
             },
             600:{
-                items:2,
-                nav:false
+                items: 2,
+                nav: false
             },
             1000:{
-                items:3,
-                nav:false
+                items: 3,
+                nav: false
             }
         }
     });
 });
-
-
-
