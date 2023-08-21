894ca745f7d7a289e6cb77fdb19aae45e6dee70e
JANDSON VITORINO <101944851+jandsonrj@users.noreply.github.com>
2023-08-21 18:51:50 -0300

    updates

--- script.js
+++ script.js
@@ -3,46 +3,56 @@ $(document).ready(function(){
         // sticky navbar on scroll script
         if(this.scrollY > 20){
             $('.navbar').addClass("sticky");
-        }else{
+        } else {
             $('.navbar').removeClass("sticky");
         }
         
         // scroll-up button show/hide script
         if(this.scrollY > 500){
             $('.scroll-up-btn').addClass("show");
-        }else{
+        } else {
             $('.scroll-up-btn').removeClass("show");
         }
     });
+
     // slide-up script
     $('.scroll-up-btn').click(function(){
         $('html').animate({scrollTop: 0});
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
+
+    // Close menu when a menu item is clicked
     $('.navbar .menu li a').click(function(){
-        // applying again smooth scroll on menu items click
+        // Close the menu by removing the "active" class
+        $('.navbar .menu').removeClass("active");
+        $('.menu-btn i').removeClass("active");
+        
+        // Applying smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
+
     // toggle menu/navbar script
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
+
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Full-Stack Web Developer/>", "Suporte Técnico", "Freelancer"],
+        strings: ["Frontend Developer/>", "Suporte Técnico", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
+
     var typed = new Typed(".typing-2", {
-        strings: ["Full-Stack Web Developer/>", "Suporte Técnico", "Freelancer"],
+        strings: ["Frontend Developer/>", "Suporte Técnico", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
-     
+
     // owl carousel script
     $('.carousel').owlCarousel({
         margin: 20,
