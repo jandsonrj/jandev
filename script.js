b1d7db57e1cd798043ba86ae7aa3bb0a1c6fa287
JANDSON VITORINO <101944851+jandsonrj@users.noreply.github.com>
2023-08-22 22:39:29 -0300

    Fix error on issue #2

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-$(document).ready(function(){
-    $(window).scroll(function(){
+$(document).ready(function () {
+    $(window).scroll(function () {
         // sticky navbar on scroll script
-        if(this.scrollY > 20){
+        if (this.scrollY > 20) {
             $('.navbar').addClass("sticky");
         } else {
             $('.navbar').removeClass("sticky");
         }
-        
+
         // scroll-up button show/hide script
-        if(this.scrollY > 500){
+        if (this.scrollY > 500) {
             $('.scroll-up-btn').addClass("show");
         } else {
             $('.scroll-up-btn').removeClass("show");
@@ -16,24 +16,24 @@ $(document).ready(function(){
     });
 
     // slide-up script
-    $('.scroll-up-btn').click(function(){
-        $('html').animate({scrollTop: 0});
+    $('.scroll-up-btn').click(function () {
+        $('html').animate({ scrollTop: 0 });
         // removing smooth scroll on slide-up button click
         $('html').css("scrollBehavior", "auto");
     });
 
     // Close menu when a menu item is clicked
-    $('.navbar .menu li a').click(function(){
+    $('.navbar .menu li a').click(function () {
         // Close the menu by removing the "active" class
         $('.navbar .menu').removeClass("active");
         $('.menu-btn i').removeClass("active");
-        
+
         // Applying smooth scroll on menu items click
         $('html').css("scrollBehavior", "smooth");
     });
 
     // toggle menu/navbar script
-    $('.menu-btn').click(function(){
+    $('.menu-btn').click(function () {
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
     });
@@ -61,18 +61,22 @@ $(document).ready(function(){
         autoplayTimeOut: 2000,
         autoplayHoverPause: true,
         responsive: {
-            0:{
+            0: {
                 items: 1,
                 nav: false
             },
-            600:{
+            600: {
                 items: 2,
                 nav: false
             },
-            1000:{
+            1000: {
                 items: 3,
                 nav: false
             }
         }
     });
 });
+
+// $('form').submit(function () {
+//     $('#confirmation-message').fadeIn();
+// });
